cacd8dd62302c5282b89f73f9054c416ce8a4d50
mohdshadab14 <mohd.shadab14@gmail.com>
2024-08-10 03:07:01 +0530

    Greedy knapsack and Job sequencing

--- greedy/JobSequencing.java
+++ greedy/JobSequencing.java
@@ -1,2 +1,66 @@
-package greedy;public class JobSequencing {
+package greedy;
+
+import java.util.*;
+
+public class JobSequencing implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        Job a = (Job)o1;
+        Job b = (Job)o2;
+        return Float.compare(b.profit, a.profit);
+    }
+
+    public static class Job{
+        private String jobName;
+        private int deadline;
+        private float profit;
+
+        private Job(String jobName,int deadline, float profit){
+            this.deadline = deadline;
+            this.jobName = jobName;
+            this.profit = profit;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Job> jobs = new ArrayList<>();
+        jobs.add( new Job("J1",5,24));
+        jobs.add( new Job("J2",3,18));
+        jobs.add( new Job("J3",3,22));
+        jobs.add( new Job("J4",2,30));
+        jobs.add( new Job("J5",4,12));
+        jobs.add( new Job("J6",2,40));
+
+        ArrayList<Job> bestSequence = greedyJobSequencing(jobs);
+        for(int i=bestSequence.size()-1;i>=0;i--){
+            System.out.println(bestSequence.get(i).jobName);
+        }
+    }
+
+    public static ArrayList<Job> greedyJobSequencing(ArrayList<Job> jobs){
+        int maxDeadline = 0;
+        for(Job job:jobs){
+            if(job.deadline>maxDeadline){
+                maxDeadline = job.deadline;
+            }
+        }
+        ArrayList<Job> bestSequence = new ArrayList<>();
+
+        Comparator comparator = new JobSequencing();
+
+        Collections.sort(jobs,comparator);
+
+        for(int i=maxDeadline;i>0;i--){
+            for(Job j :jobs){
+                if(j.deadline>=i) {
+                    bestSequence.add(j);
+                    jobs.remove(j);
+                    break;
+                }
+            }
+        }
+        return bestSequence;
+    }
 }
--- greedy/KnapsackProblem.java
+++ greedy/KnapsackProblem.java
@@ -1,2 +1,66 @@
-package greedy;public class Knapsack {
+package greedy;
+
+import java.awt.event.KeyAdapter;
+import java.util.*;
+
+public class KnapsackProblem implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        Knapsack k1 = (Knapsack) o1;
+        Knapsack k2 = (Knapsack) o2;
+        Double perUnitProfit_a = (double) (k1.profit/k1.weight);
+        Double perUnitProfit_b = (double) (k2.profit/k2.weight);
+        return perUnitProfit_b.compareTo(perUnitProfit_a);
+    }
+
+    public static class Knapsack{
+        double profit;
+        double weight;
+
+        Knapsack(double profit, double weight){
+            this.profit = profit;
+            this.weight = weight;
+        }
+    }
+
+    public static void main(String[] args) {
+        Knapsack k1 = new Knapsack(25,18);
+        Knapsack k2 = new Knapsack(24,15);
+        Knapsack k3 = new Knapsack(15,10);
+
+        ArrayList<Knapsack> knapsackArrayList = new ArrayList<>();
+        knapsackArrayList.add(k1);
+        knapsackArrayList.add(k2);
+        knapsackArrayList.add(k3);
+
+        int maxBagSize = 20;
+
+        double maximumProfit = greedyKnapsack(knapsackArrayList,maxBagSize);
+
+        System.out.println(maximumProfit);
+    }
+
+    private static double greedyKnapsack(ArrayList<Knapsack> knapsackArrayList,double maxBagSize) {
+        Comparator comparator = new KnapsackProblem();
+        Collections.sort(knapsackArrayList,comparator);
+        for(Knapsack k:knapsackArrayList){
+            System.out.println(k.weight);
+        }
+        double maxProfit = 0;
+
+        while(maxBagSize>0){
+            Knapsack k =knapsackArrayList.get(0);
+            if(k.weight<maxBagSize){
+                maxBagSize=maxBagSize-k.weight;
+                maxProfit = maxProfit+k.profit;
+                knapsackArrayList.remove(k);
+            }else{
+                double profit = maxBagSize*(k.profit/ k.weight);
+                maxProfit = maxProfit+profit;
+                maxBagSize = maxBagSize -maxBagSize;
+            }
+        }
+        return maxProfit;
+    }
+
 }
